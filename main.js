1436a2c4f5fb91e34215d45c983228a766006e50
Anurag Behura <behura960@gmail.com>
2023-04-04 02:13:07 +0530

    init

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // alert("This portfolio site is under construction!!! \n There might be some bugs or problems in this site!!! \n Click ok to continue!!!")
+window.addEventListener("load", function(){
+    this.setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block"
+        },
+        2000
+    )
+});
 
+document.querySelector("#close").addEventListener
+("click", function(){
+    document.querySelector(".popup").style.display = "none";
+
+});
